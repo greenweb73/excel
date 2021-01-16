a5bc2286f7db46e396a40a21a155cc5878e1f954
Oleg Osipov <greenweb73@gmail.com>
2021-01-16 10:57:53 +0200

    finish base table logic

--- src/module.js
+++ src/module.js
@@ -5,3 +5,9 @@ async function start() {
 }
 
 start().then(console.log)
+const el = document.createElement('div')
+el.css({ color: 'green', height: '22px' })
+function css(styles = {}) {
+  Object.keys(styles).forEach(s => el.style[s] = styles[s])
+
+}
